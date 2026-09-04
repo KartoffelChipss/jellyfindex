3a442d72c95b2da3c095a1654d69869200f7b9d4
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-09-04 18:40:21 +0200

    Add main site

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
     site: 'https://jellyfindex.com',
+    integrations: [react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
